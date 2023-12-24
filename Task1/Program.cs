8a51d7e855459b6529eed089afdde32fbbd0d66c
Sergey <spumen35@gmail.com>
2023-12-24 18:43:57 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿ // Задайте двумерный массив символов (тип char [,]). 
+ //Создать строку из символов этого массива
+// a b c => “abcdef”
+// d e f
+
+char[,] elements = new char[,]{{'a','b','c'},{'d','e','f'}};
+
+string str = "";
+string GetStringFrom2DArrayChar()
+{
+    string result="";
+    {
+        for(int i=0; i < elements.GetLength(0); i++)
+        {
+            for(int j=0; j < elements.GetLength(1); j++ )
+            {
+                result+= elements[i,j];
+            }
+        }
+        return result;
+    }
+}
+str= GetStringFrom2DArrayChar();
+Console.WriteLine(str);
+
+
+
+
